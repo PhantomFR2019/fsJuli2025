5fd82441471b99bd2a488dc56b430b4aed764c64
PhantomFR2019 <stevenmarcellino2019@gmail.com>
2025-07-05 14:47:09 +0700

    yeah

--- Algoritmapseudocode.js
+++ Algoritmapseudocode.js
@@ -27,15 +27,59 @@
  * Algoritma:
  * 1. Mulai
  * 2. Input jari-jari lingkaran
+ * 3. Tetapkan nilai π sebagai 3.14
  * 3. Hitung luas dengan rumus: luas = π * jari-jari
  * 4. Tampilkan hasil luas
  * 5. Selesai
  * Pseudocode:
  * 1. Begins
  * 2. Store Radius as input from user
- * 3. Set Area = π * Radius * Radius
- * 4. Print Area
- * 5. Ends
+ * 3. Set π (Pi) = 3.14
+ * 4. Set Area = π * Radius * Radius
+ * 5. Print Area
+ * 6. Ends
  * 
+ * 
+ * Study Case 3
+ * 
+ * Tentukan algoritma dan pseudocode untuk menghitung setengah volume bola
+ * 
+ * Algoritma:
+ * 1. Mulai
+ * 2. Input jari-jari bola
+ * 3. Tetapkan nilai π sebagai 3.14
+ * 4. Hitung volume setengah bola dengan rumus: volume = (2/3) * π * jari-jari^3
+ * 5. Tampilkan hasil volume
+ * 6. Selesai
+ * Pseudocode:
+ * 1. Begins
+ * 2. Store Radius as input from user
+ * 3. Set π (Pi) = 3.14
+ * 4. Set Volume = (2/3) * π * Radius * Radius * Radius
+ * 5. Print Volume
+ * 6. Ends
+ * 
+ * 
+ * Study Case 4
+ * 
+ * Tentukan algoritma dan pseudocode untuk menghitung luas permukaan tabung
+ * 
+ * Algoritma:
+ * 1. Mulai
+ * 2. Input jari-jari alas dan tinggi tabung
+ * 3. Tetapkan nilai π sebagai 3.14
+ * 4. Hitung luas permukaan dengan rumus: luas = 2 * π * jari-jari * (jari-jari + tinggi)
+ * 5. Tampilkan hasil luas permukaan
+ * 6. Selesai
+ * Pseudocode:
+ * 1. Begins
+ * 2. Store Radius as input from user
+ * 3. Store Height as input from user
+ * 4. Set π (Pi) = 3.14
+ * 5. Set SurfaceArea = 2 * π * Radius * (Radius + Height)
+ * 5. Print SurfaceArea
+ * 6. Ends
+ * 
+ *  
  * 
  */
