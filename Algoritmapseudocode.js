c3b6e67740f3910b8350a7ae17daa40e39a28e4e
PhantomFR2019 <stevenmarcellino2019@gmail.com>
2025-07-05 14:12:17 +0700

    Study Case Algorythm and Pseudocode

---
+++ Algoritmapseudocode.js
@@ -0,0 +1,41 @@
+// Algorythm pseudocode
+
+/**
+ * Study Case 1
+ * 
+ * tentukan algoritma dan psuedo code untuk menghitung luas persegi panjang
+ * 
+ * Algoritma:
+ * 1. Mulai
+ * 2. Input panjang dan lebar persegi panjang
+ * 3. Hitung luas dengan rumus: luas = panjang * lebar
+ * 4. Tampilkan hasil luas
+ * 5. Selesai
+ * Pseudocode:
+ * 1. Begins
+ * 2. Store Height as input from user
+ * 3. Store Width as input from user
+ * 4. Set Area = Height * Width
+ * 5. Print Area
+ * 6. Ends
+ * 
+ * 
+ * Study Case 2
+ * 
+ * Tentukan algoritma dan pseudocode untuk menghitung luas lingkaran
+ * 
+ * Algoritma:
+ * 1. Mulai
+ * 2. Input jari-jari lingkaran
+ * 3. Hitung luas dengan rumus: luas = π * jari-jari
+ * 4. Tampilkan hasil luas
+ * 5. Selesai
+ * Pseudocode:
+ * 1. Begins
+ * 2. Store Radius as input from user
+ * 3. Set Area = π * Radius * Radius
+ * 4. Print Area
+ * 5. Ends
+ * 
+ * 
+ */
