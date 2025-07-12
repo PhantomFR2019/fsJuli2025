ff6565930b3097376a8ed49103ac40ac9d1993de
PhantomFR2019 <stevenmarcellino2019@gmail.com>
2025-07-12 10:24:28 +0700

    looping

--- looping.js
+++ looping.js
@@ -1,9 +1,24 @@
-let temp = '';
+//Looping for
 
-for (let i = 0; i < 3; i++) {
-    for(let j = 0 ; j < 3; i++ ){
-        temp += 'x';
-    }
-    console.log(temp);
-    temp = '';
+for (let i = 0; i < 5; i++) {
+    console.log('ini i = ' + i);
 }
+
+// Looping while
+let j = 0;
+while (j < 5) {
+    console.log('ini j = ' + j);
+    j++;
+}
+
+let k = 0;
+let kstatus = true;
+
+while (kstatus) {
+    console.log('ini k = ' + k);
+    k++;
+    if (k === 5) {
+        kstatus = false;
+    }
+}
+
