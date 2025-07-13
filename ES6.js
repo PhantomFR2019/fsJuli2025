b40aee3b87b54599246f2318006bfcca66d7ad8f
PhantomFR2019 <stevenmarcellino2019@gmail.com>
2025-07-13 15:11:04 +0700

    .map

--- ES6.js
+++ ES6.js
@@ -42,3 +42,57 @@ console.log(products[1].price); // 200
 console.log(products[2].id); // 3
 
 products.forEach((product) => console.log(product.name)); // product 1 product 2 product 3
+
+// filter
+
+let productsFiltered = products.filter((product) => product.price > 200);
+console.log(productsFiltered); // [ { id: 3, name: 'product 3', price: 300 } ]
+
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 3.7];
+
+let evennumber = number.filter((number) => number % 2 === 0);
+console.log(evennumber); // [ 2, 4, 6, 8, 10 ]
+
+let numberlessthan3 = number.filter((number) => number < 3);
+console.log(numberlessthan3); // [ 1, 2 ]
+// % called modulus operator.
+
+let decimalnumber = number.filter((number) => number % 1 !== 0);
+console.log(decimalnumber); // [ 3.7 ]
+
+
+const products2 = [
+  { name: "kemeja", price: 250000 },
+  { name: "celana", price: 50000 },
+  { name: "sepatu", price: 80000 }
+]
+
+let productsFiltered2 = products2.filter((product) => product.price > 80000);
+console.log(productsFiltered2);
+
+
+
+// .map
+
+const number3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const doubling = number3.map((number) => number * 2);
+console.log(doubling);
+
+const evennumber2 = number3.map((number) => number % 2 === 0);
+console.log(evennumber2);
+// [ false, true, false, true, false, true, false, true, false, true ] 
+// --> shown with boolean cuz .map wont shrink the array number but always return the same length of array.
+
+const products3 = [
+  { name: "kemeja", price: 250000 },
+  { name: "celana", price: 50000 },
+  { name: "sepatu", price: 80000 }
+]
+
+// ["kemeja price 250000", "celana price 50000", "sepatu price 80000"];
+
+let productsMapped = products3.map((product) => " Price of " + product.name + " Rp. " + product.price);
+console.log(productsMapped);
+
+
+
