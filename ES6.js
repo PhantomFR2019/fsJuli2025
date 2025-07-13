dfd4d90484ceecd1b835cd7f0ef8cc9121210417
PhantomFR2019 <stevenmarcellino2019@gmail.com>
2025-07-13 14:09:35 +0700

    ES6 forEach

--- ES6.js
+++ ES6.js
@@ -15,3 +15,20 @@ console.log(multiplyArrow(6, 7)); // 42
 
 //Arrow function can't be hoisted
 
+
+// .forEach
+
+let numbers = [1, 2, 3, 4, 5];
+
+numbers.forEach((number) => console.log(number * 2)); // 2 4 6 8 10
+
+/** side note
+ * for each can show index too, but by default it's not shown
+ */
+
+// example .forEach with index
+
+numbers.forEach((number, index) => console.log(number * 2, index)); // 2 0 4 1 6 2 8 3 10 4
+
+
+
