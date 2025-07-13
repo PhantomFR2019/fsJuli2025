fb126ab9dfdff0b652515b74529c4fe4cd6fe32a
PhantomFR2019 <stevenmarcellino2019@gmail.com>
2025-07-13 13:48:16 +0700

    Arrow Function2

---
+++ ArrowFunction.js
@@ -0,0 +1,16 @@
+//Regular
+
+let multiply = (a, b) => {
+  return a * b;
+};
+
+console.log("Result:", multiply(5, 4)); // Result: 20
+
+
+// Arrow function
+
+let multiplyArrow = (a, b) => a * b;
+console.log(multiplyArrow(6, 7)); // 42
+
+
+//Arrow function can't be hoisted
