d18dc4874cb60ce5d598187d21085c7cb4375e9c
PhantomFR2019 <stevenmarcellino2019@gmail.com>
2025-07-19 09:31:12 +0700

    Class Object

---
+++ OOP.js
@@ -0,0 +1,41 @@
+/**
+ * 
+ * OOP = Object Oriented Programming
+ * 
+ * 
+ * Class & Object
+ * 
+ * let vehicle = {
+     name : "BMW",
+     type : "Car",
+     color : "Black",
+
+     startEngine : function(){
+         console.log("Engine started");
+     },
+
+     stopEngine : function(){
+         console.log("Engine stopped");
+     }
+ }
+ * 
+ * class Vehicle{
+     constructor(name, type, color){
+         this.name = name;
+         this.type = type;
+         this.color = color;
+     }
+
+     startEngine(){
+         console.log("Engine started");
+     }
+
+     stopEngine(){
+         console.log("Engine stopped");
+     }
+ }
+ * 
+ * 
+ * 
+ * 
+ */
