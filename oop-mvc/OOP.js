316037532e33203a7518ce04458c8c240f482386
PhantomFR2019 <stevenmarcellino2019@gmail.com>
2025-07-19 09:41:41 +0700

    oop-mvc

---
+++ oop-mvc/OOP.js
@@ -0,0 +1,48 @@
+/**
+ * 
+ * OOP = Object Oriented Programming
+ * 
+ * 
+ * Class & Object
+ * 
+ * let vehicle = {
+     name : "BMW",
+     type : "Car",
+     color : "Black",
+
+     startEngine : function(){
+         console.log("Engine started");
+     },
+
+     stopEngine : function(){
+         console.log("Engine stopped");
+     }
+ }
+ * 
+ * class Vehicle{
+     constructor(name, type, color){
+         this.name = name;
+         this.type = type;
+         this.color = color;
+     }
+
+     startEngine(){
+         console.log("Engine started");
+     }
+
+     stopEngine(){
+         console.log("Engine stopped");
+     }
+ }
+ * 
+ *
+ * 
+ * let vehicle = new Vehicle("BMW", "Car", "Black");
+ *
+ * --> the use of new 'objects' is called instantiation, will need to use the keyword new to create an object from a class
+ * 
+ * 
+ * 
+ * 
+ */
+
