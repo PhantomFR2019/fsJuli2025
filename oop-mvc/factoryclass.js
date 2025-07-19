c6fad00f2486f7aa7d85b9aa1bd7cda65a64e9a5
PhantomFR2019 <stevenmarcellino2019@gmail.com>
2025-07-19 11:07:55 +0700

    factory class

--- oop-mvc/factoryclass.js
+++ oop-mvc/factoryclass.js
@@ -31,12 +31,53 @@ class banana extends fruit {
 
 // factory class --> compile everything into one class, manage & edit easily
 
-class FruitFactory {
+class Supermarket {
     constructor () {
-        this.shelves = this.shelves; // array
+        this.shelves = [];
     }
+
+showItems() {
+    console.log(this.shelves);
+    if (this.shelves.length > 0) {
+        console.log("There are items in the shelves");
+    } else {
+        console.log("There are no items in the shelves");
+        }
+    }
+
+addItems (name, price, stock, category) {
+    let id;
+    if (this.shelves.length === 0) {
+        id = 1;
+    }
+    else {
+        let lastIndex = this.shelves.length - 1;
+        id = this.shelves[lastIndex].id + 1;
+    }
+    switch (category) {
+        case "Apple":
+            this.shelves.push(new apple(id, name, price, stock));
+            break;
+        case "Orange":
+            this.shelves.push(new orange(id, name, price, stock));
+            break;
+        case "Banana":
+            this.shelves.push(new banana(id, name, price, stock));
+            break;
+        default:
+    }
+    
 }
 
-const supermarket = new FruitFactory([]);
+}
 
-supermarket.shelves.push(new apple(1, "Apple", 10000, 10));
+
+const supermarket = new Supermarket();
+supermarket.showItems();
+
+supermarket.addItems("Fuji Apple", 699, 50, "Apple");
+supermarket.addItems("Royal Gala", 899, 100, "Apple");
+supermarket.addItems("Mandarin Orange", 399, 100, "Orange");
+supermarket.addItems("Sunskrit Banana", 199, 60, "Banana");
+
+supermarket.showItems();
