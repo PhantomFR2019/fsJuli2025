55cd47b84b6a0fcad4ec590f8f0816cb310ff420
PhantomFR2019 <stevenmarcellino2019@gmail.com>
2025-07-19 10:08:05 +0700

    4 main oop pillars

--- oop-mvc/OOP.js
+++ oop-mvc/OOP.js
@@ -42,7 +42,17 @@
  * --> the use of new 'objects' is called instantiation, will need to use the keyword new to create an object from a class
  * 
  * 
+ * Single use, simple, structure --> Object
+ * Reusable, scalable, structure --> Class
  * 
+ * 
+ * 
+ * 
+ * OOP 4 main pillars
+ * --> encapsulation
+ * --> inheritance
+ * --> polymorphism
+ * --> abstraction
  * 
  */
 
@@ -67,3 +77,130 @@ console.log();
 let bmw = new Vehicle("BMW", "Car", "Black", 30000);
 
 console.log(bmw);
+
+console.log();
+
+let suv = new Vehicle("SUV", "Car", "Black", 40000);
+
+console.log(suv);
+console.log();
+
+//
+
+// Inheritance
+
+class person{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+}
+
+class Employee extends person{
+    constructor(name, age, jobTitle, salary){
+        super(name, age);
+        this.jobTitle = jobTitle;
+        this.salary = salary;
+    }
+}
+
+// Polymorphism
+
+class Animal{
+    constructor(name, type){
+        this.name = name;
+        this.type = type;
+    }
+    makeSound(){
+        console.log("Animal makes sound");
+    }
+}
+
+class Dog extends Animal{
+    constructor(name, type, breed){
+        super(name, type);
+        this.breed = breed;
+    }
+    makeSound(){
+        console.log("woof woof");
+    }
+}
+
+class Cat extends Animal{
+    constructor(name, type, color){
+        super(name, type);
+        this.color = color;
+    }
+    makeSound(){
+        console.log("nyaa nyaa");
+    }
+}
+
+let dog = new Dog("Buddy", "Dog", "Golden Retriever");
+
+console.log(dog);
+dog.makeSound();
+
+console.log();
+
+let cat = new Cat("Whiskers", "Cat", "Black");
+
+console.log(cat);
+cat.makeSound();
+
+console.log();
+
+
+// Encapsulation
+
+class BankAccount{
+    constructor(accountNumber, accountHolderName, balance){
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+    }
+    deposit(amount){
+        this.balance += amount;
+    }
+    withdraw(amount){
+        if(this.balance >= amount){
+            this.balance -= amount;
+        } else {
+            console.log("Insufficient funds");
+        }
+}
+}
+
+let account = new BankAccount("123456789", "John Doe", 1000);
+
+console.log(account);
+
+account.deposit(500);
+
+console.log(account);
+
+account.withdraw(2000);
+
+console.log(account);
+
+// Abstraction
+
+class army {
+    constructor(name, rank){
+        this.name = name;
+        this.rank = rank;
+    }
+    promote(newRank){
+        this.rank = newRank;
+    }
+}
+
+let soldier = new army("John", "Private");
+
+console.log(soldier);
+
+soldier.promote("Sergeant");
+
+console.log(soldier);
+
+
