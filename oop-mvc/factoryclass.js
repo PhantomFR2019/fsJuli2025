1610128e1063b990d8c0cf1857852e9553c24411
PhantomFR2019 <stevenmarcellino2019@gmail.com>
2025-07-20 09:21:07 +0700

    MVC

--- oop-mvc/factoryclass.js
+++ oop-mvc/factoryclass.js
@@ -62,15 +62,29 @@ class Supermarket {
     }
     switch (category) {
       case "Apple":
-        this.shelves.push(new Apple(id, name, price, stock));
+        this.shelves.push(new apple(id, name, price, stock));
         break;
       case "Orange":
-        this.shelves.push(new Orange(id, name, price, stock));
+        this.shelves.push(new orange(id, name, price, stock));
         break;
       case "Banana":
-        this.shelves.push(new Banana(id, name, price, stock));
+        this.shelves.push(new banana(id, name, price, stock));
         break;
     }
+  }
+  deleteItem(id) {
+    this.shelves = this.shelves.filter((shelf) => shelf.id !== id);
+  }
+  editItem(id, name, price, stock, category) {
+    this.shelves = this.shelves.map((shelf) => {
+      if (shelf.id === id) {
+        shelf.name = name;
+        shelf.price = price;
+        shelf.stock = stock;
+        shelf.category = category;
+      }
+      return shelf;
+    });
   }
 }
 
@@ -80,6 +94,17 @@ supermarket.addItem("Pokan", 50000, 5, "Orange");
 supermarket.addItem("Cavendish Sunpride", 35000, 10, "Banana");
 supermarket.addItem("Queen Elizabeth", 105000, 17, "Apple");
 supermarket.addItem("Pisang Barangan", 15000, 10, "Banana");
+
+
+//supermarket.deleteItem(2);
+//supermarket.showItems();
+
+supermarket.editItem(1, "Gala Royale Apple", 75000, 10, "Apple");
+supermarket.editItem(3, "Cavendish Sunpride Banana", 35000, 10, "Banana");
 supermarket.showItems();
 // supermarket.showItemsInTable();
 // console.log(supermarket);
+
+console.log();
+
+
